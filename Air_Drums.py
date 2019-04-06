c866a6d692a5701925ec46320ffec28e92173299
kaustubh sadekar <42736936+kaustubh-sadekar@users.noreply.github.com>
2019-04-06 11:58:23 +0530

    Update Air_Drums.py
    indentation and image path modified

--- Air_Drums.py
+++ Air_Drums.py
@@ -55,8 +55,8 @@ H,W = frame.shape[:2]
 kernel = np.ones((7,7),np.uint8)
 
 # reading the image of hatt and snare for augmentation.
-Hatt = cv2.resize(cv2.imread('Hatt.png'),(200,100),interpolation=cv2.INTER_CUBIC)
-Snare = cv2.resize(cv2.imread('Snare.png'),(200,100),interpolation=cv2.INTER_CUBIC)
+Hatt = cv2.resize(cv2.imread('./Images/Hatt.png'),(200,100),interpolation=cv2.INTER_CUBIC)
+Snare = cv2.resize(cv2.imread('./Images/Snare.png'),(200,100),interpolation=cv2.INTER_CUBIC)
 
 
 # Setting the ROI area for blue color detection
@@ -78,38 +78,39 @@ while True:
 	# grab the current frame
 	ret, frame = camera.read()
 	frame = cv2.flip(frame,1)
-    if not ret:
-    	break
-    
-    # Selecting ROI corresponding to snare
-    snare_ROI = np.copy(frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]])
-    mask = ROI_analysis(snare_ROI,1)
 
-    # Selecting ROI corresponding to Hatt
-    hatt_ROI = np.copy(frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]])
-    mask = ROI_analysis(hatt_ROI,2)
+	if not(ret):
+		break
+    
+	# Selecting ROI corresponding to snare
+	snare_ROI = np.copy(frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]])
+	mask = ROI_analysis(snare_ROI,1)
 
-    # A writing text on an image.
-    cv2.putText(frame,'Project: Air Drums',(10,30),2,1,(20,20,20),2)
+	# Selecting ROI corresponding to Hatt
+	hatt_ROI = np.copy(frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]])
+	mask = ROI_analysis(hatt_ROI,2)
+
+	# A writing text on an image.
+	cv2.putText(frame,'Project: Air Drums',(10,30),2,1,(20,20,20),2)
     
-    # Display the ROI to view the blue colour being detected
-    if Verbsoe:
-    	# Displaying the ROI in the Image
-    	frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]] = cv2.bitwise_and(frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]],frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]], mask=mask[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]])
-    	frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]] = cv2.bitwise_and(frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]],frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]],mask=mask[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]])
+	# Display the ROI to view the blue colour being detected
+	if Verbsoe:
+		# Displaying the ROI in the Image
+		frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]] = cv2.bitwise_and(frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]],frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]], mask=mask[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]])
+		frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]] = cv2.bitwise_and(frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]],frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]],mask=mask[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]])
     
-    # Augmenting the instruments in the output frame.
-    else:
-    	# Augmenting the image of the instruments on the frame.
-    	frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]] = cv2.addWeighted(Snare, 1, frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]], 1, 0)
-    	frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]] = cv2.addWeighted(Hatt, 1, frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]], 1, 0)
+	# Augmenting the instruments in the output frame.
+	else:
+		# Augmenting the image of the instruments on the frame.
+		frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]] = cv2.addWeighted(Snare, 1, frame[Snare_top[1]:Snare_btm[1],Snare_top[0]:Snare_btm[0]], 1, 0)
+		frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]] = cv2.addWeighted(Hatt, 1, frame[Hatt_top[1]:Hatt_btm[1],Hatt_top[0]:Hatt_btm[0]], 1, 0)
     
     
-    cv2.imshow('Output',frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
+	cv2.imshow('Output',frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
  
 # cleanup the camera and close any open windows
 camera.release()
